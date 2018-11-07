0437e6cdae8166d9e3db4f300cf1a93344c4132f
GadgetReboot <34284866+GadgetReboot@users.noreply.github.com>
2018-11-07 00:20:52 -0500

    Update Arduino_Sketch_Analog_Serial.ino

--- Simple_Scope/Arduino_Sketch_Analog_Serial.ino
+++ Simple_Scope/Arduino_Sketch_Analog_Serial.ino
@@ -1,3 +1,7 @@
+// This sketch reads analog data on A0 and sends it out to 
+// the serial port with each sample terminated by a new line "\n"
+// The Processing sketch can then parse the data and plot it graphically.
+
 void setup() {
   Serial.begin(9600);
 }
